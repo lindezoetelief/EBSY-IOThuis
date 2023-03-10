c9851007287ca872d4e97fc0d36b9d21d1684901
lindezoetelief <87785@roc-teraa.nl>
2023-03-10 12:44:41 +0100

    Meet temp en hum + laat dit zien op lcd wanneer knop wordt ingedrukt.

--- IOT-huis/IOT-huis.ino
+++ IOT-huis/IOT-huis.ino
@@ -1,10 +1,13 @@
+//Libraries
 #include <dht.h>
-#include <LiquidCrystal.h>
+#include "Wire.h" 
+#include "LiquidCrystal_I2C.h"
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
 dht DHT;
 
 #define DHT11_PIN 2
 
-LiquidCrystal lcd(I2C_1);
 //Leds
 int led1Pin = 10;
 int led2Pin = 9;
@@ -34,12 +37,13 @@ void setup() {
   buttonState = digitalRead(switch1Pin);
   button2State = digitalRead(switch2Pin);
   Serial.begin(9600);
-  lcd.begin(16, 2);
-  lcd.print("hello, world!");
+  
+  lcd.init();
+  lcd.clear();         
+  lcd.backlight();      // Make sure backlight is on
 }
 
-void loop() {
-
+void loop() { 
   //Red button turns on green Led
   val = digitalRead(switch1Pin);
   val2 = digitalRead(switch1Pin);
@@ -57,17 +61,33 @@ void loop() {
         {
           lightMode = 1;
           digitalWrite(led1Pin, HIGH);
+          //Print temperture and humidity to serial monitor and lcd 
+          int chk = DHT.read11(DHT11_PIN);
+          Serial.print("Temperature = ");
+          Serial.println(DHT.temperature);
+          Serial.print("Humidity = ");
+          Serial.println(DHT.humidity);
+          lcd.setCursor(0,0); 
+          lcd.print("Temp: ");
+          lcd.print(DHT.temperature);
+          lcd.print((char)223);
+          lcd.print("C");
+          lcd.setCursor(0,1);
+          lcd.print("Humidity: ");
+          lcd.print(DHT.humidity);
+          lcd.print("%");
         }
         else
         {
           lightMode = 0;
           digitalWrite(led1Pin, LOW);
+          lcd.clear();
         }
       }
     }
   }
   buttonState = val;
-
+  
   //Blue button turns on yellow Led
   val3 = digitalRead(switch2Pin);
   val4 = digitalRead(switch2Pin);
@@ -85,32 +105,30 @@ void loop() {
         {
           lightMode = 1;
           digitalWrite(led2Pin, HIGH);
-        }
+          //Print temperture and humidity to serial monitor and lcd 
+          int chk = DHT.read11(DHT11_PIN);
+          Serial.print("Temperature = ");
+          Serial.println(DHT.temperature);
+          Serial.print("Humidity = ");
+          Serial.println(DHT.humidity);
+          lcd.setCursor(0,0); 
+          lcd.print("Temp: ");
+          lcd.print(DHT.temperature);
+          lcd.print((char)223);
+          lcd.print("C");
+          lcd.setCursor(0,1);
+          lcd.print("Humidity: ");
+          lcd.print(DHT.humidity);
+          lcd.print("%");
+          }
         else
         {
           lightMode = 0;  
           digitalWrite(led2Pin, LOW);
+          lcd.clear();
         }
       }
     }
   }
   button2State = val3;
-
-  int chk = DHT.read11(DHT11_PIN);
-  Serial.print("Temperature = ");
-  Serial.println(DHT.temperature);
-  Serial.print("Humidity = ");
-  Serial.println(DHT.humidity);
-  delay(1000);
-
-  lcd.setCursor(0,0); 
-  lcd.print("Temp: ");
-  lcd.print(DHT.temperature);
-  lcd.print((char)223);
-  lcd.print("C");
-  lcd.setCursor(0,1);
-  lcd.print("Humidity: ");
-  lcd.print(DHT.humidity);
-  lcd.print("%");
-  delay(1000);
 }
